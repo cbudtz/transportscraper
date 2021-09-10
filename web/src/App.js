043f9e34b149e4dd0593077288ec668d35852b21
Christian <chrbudtz@gmail.com>
2021-09-10 22:36:35 +0200

    Multipage scrape

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useState} from "react";
 import {Button, Grid, TextField} from "@material-ui/core";
-// import {JSDOM} from "jsdom";
+import * as jsonexport from "jsonexport/dist"
 
 const BASE_URL = process.env.NODE_ENV==="development" ? "http://localhost:5000/" : "/"
 
@@ -9,21 +9,50 @@ function App() {
     const [page, setPage] = useState(1);
     const [scrape, setScrape] = useState("");
     async function scrapeUrl() {
-        const res = await fetch(BASE_URL+page)
-        const text = await res.text();
-        var dom = new DOMParser().parseFromString(text,"text/html")
-        console.log(dom.getElementById("results"));
         let jobArray = []
-        var resultArray = dom.getElementById("results").getElementsByClassName("jobsearch-result");
-        for (let i = 0 ; i < resultArray.length; i ++){
-            let job = resultArray.item(i)
-            console.log(job)
-            const title = job.getElementsByClassName("PaidJob-inner").item(0).getElementsByTagName("a").item(1).innerText;
-            const description = job.getElementsByClassName("PaidJob-inner").item(0).innerText
-            const time = job.getElementsByTagName("time").item(0).innerText
-            jobArray.push({title: title,description:description, time:time})
+        for (let i=page;i <200;i++ ) {
+            const res = await fetch(BASE_URL + i)
+            const text = await res.text();
+            var dom = new DOMParser().parseFromString(text, "text/html")
+      //      console.log(dom.getElementById("results"));
+
+            var resultArray = dom.getElementById("results").getElementsByClassName("jobsearch-result");
+            if (resultArray.length<1) {break;}
+            for (let i = 0; i < resultArray.length; i++) {
+                let job = resultArray.item(i)
+                console.log(job)
+                let title = job.getElementsByClassName("PaidJob-inner")?.item(0)?.getElementsByTagName("a").item(1).innerText;
+                title = title || job.getElementsByClassName("jix_robotjob-inner").item(0)?.getElementsByTagName("a").item(0).innerText.trim();
+                let description = job.getElementsByClassName("PaidJob-inner")?.item(0)?.innerText;
+                description = description || job.getElementsByClassName("jix_robotjob-inner").item(0)?.innerText.trim();
+                const time = job.getElementsByTagName("time")?.item(0)?.innerText
+                jobArray.push({title: title, description: description, time: time})
+            }
+            await (async ()=>setPage(page+i))()
         }
         setScrape(JSON.stringify(jobArray))
+        const repJSON = JSON.parse(JSON.stringify(jobArray).replace("\n\n","  "))
+        const csv = await jsonexport(repJSON,{rowDelimiter: ";"},(err,csv)=>{
+            if (err) return console.error(err);
+            console.log(csv)
+            setScrape(csv)
+        })
+    }
+
+    function saveTextAsFile(textToWrite, fileNameToSaveAs)
+    {
+        var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+        var downloadLink = document.createElement("a");
+        downloadLink.download = fileNameToSaveAs;
+        downloadLink.innerHTML = "Download File";
+        if (window.webkitURL != null)
+        {
+            // Chrome allows the link to be clicked
+            // without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        }
+
+        downloadLink.click();
     }
 
     return (
@@ -31,14 +60,21 @@ function App() {
             <Grid container>
                 <Grid item xs={1}> </Grid>
                 <Grid item xs={10} >
-                    <TextField fullWidth value={page} onChange={(e)=>setPage(e.target.value)} />
                     <Button onClick={scrapeUrl}>Scrape</Button>
+                    <div>Scraping page: {page}</div>
                 </Grid>
                 <Grid item xs={1}> </Grid>
                 <Grid item xs={1}> </Grid>
                 <Grid item xs={10}>
-                        <TextField fullWidth multiline minRows={6} value={scrape}/>
+                    <Button onClick={()=>saveTextAsFile(scrape,"scrape.csv")}>Download</Button>
                 </Grid>
+                <Grid item xs={1}> </Grid>
+                <Grid item xs={1}> </Grid>
+
+                <Grid item xs={10} >
+                    <pre style={{whiteSpace:"pre-wrap",wordWrap:"break-word"}}>{scrape}</pre>
+                </Grid>
+
             </Grid>
         </div>
     );
